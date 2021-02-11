f925efa8f7b1158b59ae9d62c2c9e00b11d59326
d-otis <dan@dan-foley.com>
2021-02-11 17:16:49 -0500

    refactor requires to follow tutorial

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 const path = require('path')
 
 app.get('/', (req, res) => {
